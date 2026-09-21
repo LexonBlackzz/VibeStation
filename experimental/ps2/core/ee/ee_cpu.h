e7e8baf9cfb3fab6c7de5abe0fb6d09efe386732
mappazinho <mappazinho@gmail.com>
2026-09-21 20:40:44 +0300

    ps2: add VU0 accumulator state

--- experimental/ps2/core/ee/ee_cpu.h
+++ experimental/ps2/core/ee/ee_cpu.h
@@ -36,6 +36,7 @@ struct EeCpuState {
     std::array<u32, 32> fcr{};
     u32 fpu_acc = 0;
     std::array<EeGpr, 32> vu_vf{};
+    EeGpr vu_acc{};
     std::array<u32, 32> vu_vi{};
     u64 instructions_executed = 0;
     u32 last_pc = 0;
