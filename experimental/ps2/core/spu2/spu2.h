f650628fba0ca9c13abd03b13f98eb6a84114320
LexonBlackzz <98592761+LexonBlackzz@users.noreply.github.com>
2026-09-24 00:00:11 +0300

    fix(ps2-spu2): unify audio diagnostic counters

--- experimental/ps2/core/spu2/spu2.h
+++ experimental/ps2/core/spu2/spu2.h
@@ -15,19 +15,19 @@ class Spu2 {
 public:
     struct DebugStats {
         u64 register_writes = 0;
+        u64 dma_write_halfwords = 0;
+        u64 dma_read_halfwords = 0;
         u64 key_on_writes = 0;
         u64 key_off_writes = 0;
-        u64 mixed_frames = 0;
-        u64 mixer_frames_with_active_voice = 0;
-        u64 nonzero_output_frames = 0;
         u64 keyed_on_voices = 0;
         u64 keyed_off_voices = 0;
         u64 decoded_blocks = 0;
         u64 decoded_nonzero_samples = 0;
-        u64 dma_write_halfwords = 0;
-        u64 dma_read_halfwords = 0;
-        u32 max_active_voices = 0;
+        u64 mixed_frames = 0;
+        u64 mixer_frames_with_active_voice = 0;
+        u64 nonzero_output_frames = 0;
         u32 decoded_peak = 0;
+        u32 max_active_voices = 0;
         u32 pre_master_peak = 0;
         u32 output_peak = 0;
     };
