5993e6beb27ebda120375d210b07d4df2b07ac02
LexonBlackzz <98592761+LexonBlackzz@users.noreply.github.com>
2026-09-24 00:12:00 +0300

    debug(ps2-sif): track OSDSND sequencer RPCs

--- experimental/ps2/core/dma/sif_dma.h
+++ experimental/ps2/core/dma/sif_dma.h
@@ -52,9 +52,14 @@ public:
 
         u64 rpc_calls = 0;
         u64 sound_rpc_calls = 0;
+        u64 sound_st_init_calls = 0;
         u64 sound_bgm_play_calls = 0;
+        u64 sound_bgm_stop_calls = 0;
         u64 sound_bgm_open_calls = 0;
+        u64 sound_tick_mode_calls = 0;
+        u64 sound_master_volume_calls = 0;
         u64 sound_timer_start_calls = 0;
+        u64 sound_se_play_calls = 0;
         u64 sound_set_param_calls = 0;
         u64 sound_set_switch_calls = 0;
         u64 sound_set_addr_calls = 0;
