face97c0801d8a3dd4544c29f5783c0df488d962
mappazinho <mappazinho@gmail.com>
2026-09-24 13:08:36 +0300

    PS2: reset heap-backed EE block cache

--- experimental/ps2/core/ps2_system.cpp
+++ experimental/ps2/core/ps2_system.cpp
@@ -247,7 +247,10 @@ void Ps2System::reset(u32 entry_point) {
     quiet_block_instructions_ = 0;
     quiet_block_hits_ = 0;
     quiet_block_compiles_ = 0;
-    quiet_ee_blocks_.fill(QuietEeBlock{});
+    std::fill(
+        quiet_ee_blocks_.begin(),
+        quiet_ee_blocks_.end(),
+        QuietEeBlock{});
     idle_skip_reasons_.fill(0);
 }
 Ps2System::QuietEeBlock* Ps2System::quiet_ee_block(u32 pc) {
