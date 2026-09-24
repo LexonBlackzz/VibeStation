8897067f2a93c5566ce76af5eae7ffda44df3f44
mappazinho <mappazinho@gmail.com>
2026-09-24 18:05:06 +0300

    PS2: expose next EE timer IRQ boundary

--- experimental/ps2/core/hw/ee_hw.h
+++ experimental/ps2/core/hw/ee_hw.h
@@ -17,15 +17,9 @@ public:
     [[nodiscard]] u32 vif1_stat() const;
     [[nodiscard]] bool intc_pending() const;
     [[nodiscard]] bool dmac_pending() const;
+    [[nodiscard]] u64 cycles_to_timer_irq() const;
     [[nodiscard]] bool timer_irq_possible() const {
-        const u32 mask = generic_read32(kRegBase + 0x10u);
-        for (u32 i = 0; i < timer_mode_.size(); ++i) {
-            if ((mask & (1u << (9u + i))) != 0 &&
-                (timer_mode_[i] & ((1u << 8) | (1u << 9))) != 0) {
-                return true;
-            }
-        }
-        return false;
+        return cycles_to_timer_irq() != ~u64{0};
     }
     [[nodiscard]] bool dmac_enabled() const {
         return (dmac_regs_[0x6000u] & 1u) != 0;
