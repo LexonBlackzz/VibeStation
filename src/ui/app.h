452574592f582136bb00ce590cd317bc471fdf0c
mappazinho <mappazinho@gmail.com>
2026-09-25 17:38:31 +0300

    ui: track floating toolbar rewind hold state

--- src/ui/app.h
+++ src/ui/app.h
@@ -74,6 +74,7 @@ private:
 	bool show_bindings_config_ = false;
 	bool show_fmv_diagnostics_ = false;
 	bool gameplay_toolbar_turbo_active_ = false;
+	bool gameplay_toolbar_rewind_active_ = false;
 	std::string bios_path_;
 	std::string rom_directory_;
 	std::string game_bin_path_;
